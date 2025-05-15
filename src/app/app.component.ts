8d6bce289e75e9803c2faca2a407ed34dcee19e6
dat truong <thelegendaglx@gmail.com>
2025-05-15 18:47:09 +0700

    stil not working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { exampleSetup } from 'prosemirror-example-setup';
 import { pageNodeSpec } from './prosemirror-nodes/page';
 import { pageBreakPlugin } from './prosemirror-plugin/page-break-plugin';
 import { pageSchema } from './prosemirror-schema/page-schema';
-
+import { Node } from 'prosemirror-model';
 @Component({
   selector: 'app-root',
   imports: [],
@@ -16,6 +16,9 @@ import { pageSchema } from './prosemirror-schema/page-schema';
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements AfterViewInit {
+  exportJSon() {
+    console.log(this.view?.state.doc.toJSON());
+  }
   mySchema = pageSchema;
   view?: EditorView;
   pageNumber: number = 1;
@@ -26,16 +29,31 @@ export class AppComponent implements AfterViewInit {
   // Mix the nodes from prosemirror-schema-list into the basic schema to
   // create a schema with list support.
   ngAfterViewInit(): void {
-    const doc = schema.nodes.doc.create({
+    const doc = Node.fromJSON(this.mySchema, {
+      type: 'doc',
       content: [
         {
           type: 'page',
           attrs: { 'page-number': '1' },
-          class: 'page',
           content: [
             {
               type: 'paragraph',
-              content: [{ type: 'text', text: 'This is page 1.' }],
+              content: [
+                {
+                  type: 'text',
+                  text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam blandit arcu ut odio iaculis, eu facilisis massa varius. Praesent dolor nibh, laoreet ac nisi id, placerat gravida justo. Quisque ac felis dictum nulla ornare posuere. Interdum et malesuada fames ac ante ipsum primis in faucibus. Quisque pretium lacinia justo quis hendrerit. Phasellus tortor risus, dapibus sed nunc ut, lacinia lobortis turpis. Praesent porttitor leo at libero dapibus auctor. Sed ac molestie tellus. Etiam ultrices magna quis pulvinar blandit. Phasellus bibendum consequat tincidunt. Quisque sed sem sed lacus volutpat placerat. Sed nisi lacus, aliquet ac lobortis ut, iaculis quis ipsum. Nam sagittis tortor sit amet scelerisque vulputate. Aenean mollis et enim at ultricies. Etiam ac sollicitudin elit, eget sollicitudin ligula. Integer id urna vehicula, cursus mauris sit amet, tristique lacus. Nunc faucibus ex nec enim dapibus vehicula. Nunc at leo laoreet, pharetra lacus ut, ultricies urna. Cras vitae finibus tellus. Praesent quis nunc id purus gravida tempor. Fusce ac felis venenatis, luctus arcu elementum, tempor neque. Nullam ut erat id purus tincidunt dictum. Curabitur imperdiet vel augue nec ornare. In hac habitasse platea dictumst. Proin pulvinar augue sapien, eget porttitor tellus pharetra ac. Nam sit amet nunc et lectus sodales dictum non ut leo. Maecenas vestibulum justo nec lacinia rutrum. Maecenas in fermentum lectus, sed aliquam ipsum. Aliquam interdum sollicitudin mi, vel placerat magna commodo et. Nunc sit amet est mauris. Nam accumsan ullamcorper tortor et mollis. Nunc accumsan est nec varius pulvinar. Vivamus feugiat volutpat tortor non volutpat. Mauris et felis in justo porttitor maximus faucibus sed enim. Etiam imperdiet sed turpis et vulputate. Morbi posuere ex nec dui vulputate, sed consequat felis venenatis. Duis consectetur sodales arcu. Pellentesque laoreet malesuada nunc nec rutrum. Suspendisse tincidunt sed nunc vitae varius. Ut pellentesque ipsum ac pulvinar porttitor. Duis et elit a odio auctor venenatis. Curabitur sit amet leo et magna fermentum accumsan. Pellentesque interdum hendrerit ex et vulputate. Suspendisse condimentum aliquet mi at placerat. Aliquam dapibus magna ut urna euismod, congue condimentum nibh scelerisque. Nam iaculis lobortis nisi, nec facilisis lorem rhoncus nec. Aenean blandit lorem in auctor egestas. In vehicula, lorem ac tempus lacinia, velit mauris dignissim odio, venenatis faucibus eros lorem accumsan mauris. Maecenas risus nisl, aliquet id mi eu, iaculis mattis ante. Aliquam nec tincidunt felis, vitae venenatis ligula. Integer eleifend, justo ac mattis sagittis, tortor lorem ornare nibh, at tempor enim nisi vitae diam. Praesent venenatis commodo orci a tristique. Nulla ex nibh, laoreet ut quam quis, rhoncus faucibus magna.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam blandit arcu ut odio iaculis, eu facilisis massa varius. Praesent dolor nibh, laoreet ac nisi id, placerat gravida justo. Quisque ac felis dictum nulla ornare posuere. Interdum et malesuada fames ac ante ipsum primis in faucibus. Quisque pretium lacinia justo quis hendrerit. Phasellus tortor risus, dapibus sed nunc ut, lacinia lobortis turpis. Praesent porttitor leo at libero dapibus auctor. Sed ac molestie tellus. Etiam ultrices magna quis pulvinar blandit. Phasellus bibendum consequat tincidunt. Quisque sed sem sed lacus volutpat placerat. Sed nisi lacus, aliquet ac lobortis ut, iaculis quis ipsum. Nam sagittis tortor sit amet scelerisque vulputate. Aenean mollis et enim at ultricies. Etiam ac sollicitudin elit, eget sollicitudin ligula. Integer id urna vehicula, cursus mauris sit amet, tristique lacus. Nunc faucibus ex nec enim dapibus vehicula. Nunc at leo laoreet, pharetra lacus ut, ultricies urna. Cras vitae finibus tellus. Praesent quis nunc id purus gravida tempor. Fusce ac felis venenatis, luctus arcu elementum, tempor neque. Nullam ut erat id purus tincidunt dictum. Curabitur imperdiet vel augue nec ornare. In hac habitasse platea dictumst. Proin pulvinar augue sapien, eget porttitor tellus pharetra ac. Nam sit amet nunc et lectus sodales dictum non ut leo. Maecenas vestibulum justo nec lacinia rutrum. Maecenas in fermentum lectus, sed aliquam ipsum. Aliquam interdum sollicitudin mi, vel placerat magna commodo et. Nunc sit amet est mauris. Nam accumsan ullamcorper tortor et mollis. Nunc accumsan est nec varius pulvinar. Vivamus feugiat volutpat tortor non volutpat. Mauris et felis in justo porttitor maximus faucibus sed enim. Etiam imperdiet sed turpis et vulputate. Morbi posuere ex nec dui vulputate, sed consequat felis venenatis. Duis consectetur sodales arcu. Pellentesque laoreet malesuada nunc nec rutrum. Suspendisse tincidunt sed nunc vitae varius. Ut pellentesque ipsum ac pulvinar porttitor. Duis et elit a odio auctor venenatis. Curabitur sit amet leo et magna fermentum accumsan. Pellentesque interdum hendrerit ex et vulputate. Suspendisse condimentum aliquet mi at placerat. Aliquam dapibus magna ut urna euismod, congue condimentum nibh scelerisque. Nam iaculis lobortis nisi, nec facilisis lorem rhoncus nec. Aenean blandit lorem in auctor egestas. In vehicula, lorem ac tempus lacinia, velit mauris dignissim odio, venenatis faucibus eros lorem accumsan mauris. Maecenas risus nisl, aliquet id mi eu, iaculis mattis ante. Aliquam nec tincidunt felis, vitae venenatis ligula. Integer eleifend, justo ac mattis sagittis, tortor lorem ornare nibh, at tempor enim nisi vitae diam. Praesent venenatis commodo orci a tristique. Nulla ex nibh, laoreet ut quam quis, rhoncus faucibus magna.',
+                },
+              ],
+            },
+          ],
+        },
+        {
+          type: 'page',
+          attrs: { 'page-number': '2' },
+          content: [
+            {
+              type: 'paragraph',
+              content: [{ type: 'text', text: 'page 2' }],
             },
           ],
         },
@@ -48,8 +66,20 @@ export class AppComponent implements AfterViewInit {
         // doc: DOMParser.fromSchema(this.mySchema).parse(
         //   document.querySelector('#content') as Node
         // ),
-        plugins: [...exampleSetup({ schema: this.mySchema }), pageBreakPlugin],
+        plugins: [
+          ...exampleSetup({ schema: this.mySchema }),
+          pageBreakPlugin(this.view),
+        ],
       }),
+    });
+
+    document.addEventListener('click', (event) => {
+      console.log(event.target);
+
+      console.log(
+        'posAtDOM: ',
+        this.view?.posAtDOM(event.target as any, 0, -1)
+      );
     });
   }
 
--- src/app/prosemirror-plugin/page-break-plugin.ts
+++ src/app/prosemirror-plugin/page-break-plugin.ts
@@ -1,10 +1,14 @@
-import { Plugin } from 'prosemirror-state';
+import { EditorState, Plugin, Transaction } from 'prosemirror-state';
 import { pageSchema } from '../prosemirror-schema/page-schema';
 import { EditorView } from 'prosemirror-view';
-
-// Create a custom plugin that checks for page overflow after each transaction
-export const pageBreakPlugin = new Plugin({
-  appendTransaction(transactions, oldState, newState) {
+import { Node } from 'prosemirror-model';
+import { nodes } from 'prosemirror-schema-basic';
+const breakPageByText = (text: string) => {
+  return function (
+    transactions: readonly Transaction[],
+    oldState: EditorState,
+    newState: EditorState
+  ) {
     for (let index = 0; index < transactions.length; index++) {
       const transaction = transactions[index];
       if (!transaction.docChanged) {
@@ -31,5 +35,319 @@ export const pageBreakPlugin = new Plugin({
       }
     }
     return null;
-  },
-});
+  };
+};
+
+const breakPageBySize = (view: EditorView | undefined) => {
+  return function (
+    transactions: readonly Transaction[],
+    oldState: EditorState,
+    newState: EditorState
+  ) {
+    for (let index = 0; index < transactions.length; index++) {
+      const transaction = transactions[index];
+      if (!transaction.docChanged) {
+        return;
+      }
+
+      let currentPage = null;
+      newState.doc.descendants((node, pos) => {
+        if (node.type.name === 'page') {
+          // Detect the position of the page node that has been modified
+          // (Check if the current node was affected by the transaction)
+          if (transaction.mapping.map(pos) !== pos) {
+            // This page node was modified in the transaction
+            currentPage = pos;
+            return; // Break the iteration
+          }
+        }
+      });
+
+      console.log('currentPage', currentPage);
+      if (!view) {
+        return null;
+      }
+      const $from = transaction.selection.$from;
+      const node = view.nodeDOM($from.depth);
+      // if (transaction.doc.textContent.includes('break page')) {
+      //   const newPage = pageSchema.nodes['page'].create({ 'page-number': 2 }, [
+      //     pageSchema.nodes['paragraph'].create(),
+      //   ]);
+      //   const position = newState.doc.content.size;
+      //   let trWithPage = newState.tr
+      //     // delete break page
+      //     .delete(
+      //       transaction.selection.$from.pos - 'break page'.length,
+      //       transaction.selection.$from.pos
+      //     )
+      //     // add new page
+      //     .insert(position - 'break page'.length, newPage);
+      //   console.log(trWithPage);
+      //   // Return the updated transaction (apply this change)
+      //   return trWithPage;
+      // } else {
+      //   console.log('no break');
+      // }
+    }
+    return null;
+  };
+};
+// Create a custom plugin that checks for page overflow after each transaction
+export function pageBreakPlugin(view: EditorView | undefined) {
+  let editorView: EditorView;
+
+  return new Plugin({
+    view(view) {
+      editorView = view;
+      return {};
+    },
+    //appendTransaction: breakPageByText('break page'),
+    appendTransaction(
+      transactions: readonly Transaction[],
+      oldState: EditorState,
+      newState: EditorState
+    ) {
+      for (let index = 0; index < transactions.length; index++) {
+        const transaction = transactions[index];
+        if (!transaction.docChanged) {
+          return;
+        }
+
+        let pagePosition = 0;
+        // for testing
+        // console.log('parent', transaction.selection.$from.parent);
+        // console.log('parentOffset', transaction.selection.$from.parentOffset);
+        // console.log('pos', transaction.selection.$from.pos);
+
+        // console.log(
+        //   'dom by  pos - parentOffset',
+        //   editorView.nodeDOM(
+        //     transaction.selection.$from.pos -
+        //       transaction.selection.$from.parentOffset -
+        //       1
+        //   )
+        // );
+
+        const parentNode = transaction.selection.$from.parent;
+        let newParentNodePosition = 0;
+
+        let currentPage: Node | undefined;
+        let currentPagePosition = 0;
+
+        let nextPageNumber = 0;
+        let nextPage: Node | undefined;
+        let nextPagePosition = 0;
+        newState.doc.descendants((node, pos) => {
+          // Getting current parent position
+          if (node.eq(parentNode)) {
+            newParentNodePosition = transaction.mapping.map(pos);
+          }
+
+          newState.doc.descendants((pageNode, pagePosition) => {
+            // Finding current page
+            if (
+              pageNode.type.name === 'page' &&
+              pagePosition < newParentNodePosition &&
+              pagePosition + pageNode.nodeSize > newParentNodePosition
+            ) {
+              currentPage = pageNode;
+              currentPagePosition = pagePosition;
+              const currentPageNumber = parseInt(
+                currentPage.attrs['page-number'],
+                10
+              );
+              nextPageNumber = currentPageNumber + 1;
+            }
+            // Finding next page
+            if (
+              currentPage &&
+              nextPageNumber &&
+              pageNode.attrs['page-number'] === nextPageNumber.toString()
+            ) {
+              nextPage = pageNode;
+              nextPagePosition = pagePosition;
+            }
+          });
+        });
+
+        console.log(
+          'newParentNodePosition',
+          newParentNodePosition,
+          'currentPage',
+          currentPage,
+          'currentPagePosition',
+          currentPagePosition,
+          'nextPage',
+          nextPage,
+          'nextPagePosition',
+          nextPagePosition
+        );
+
+        if (!currentPage) {
+          console.error('ERROR. Not found current page.');
+          return;
+        }
+
+        const currentPageDOM = editorView.dom.querySelector(
+          `div[page-number='${currentPage.attrs['page-number']}']`
+        ) as any;
+
+        if (!currentPageDOM) {
+          console.error('ERROR. Not found current page DOM.');
+          return;
+        }
+
+        const { scrollHeight, clientHeight } = currentPageDOM;
+        // the page is overflowed then we need to break the current page
+        if (scrollHeight > clientHeight) {
+          let nextPageTransaction = newState.tr;
+          if (nextPagePosition > 0) {
+            // Next page is existed so we just move the current block to the new page
+            const blockPosition = transaction.selection.$from;
+            const block = blockPosition.parent.toJSON();
+            return newState.tr
+              .insert(
+                nextPagePosition + 1,
+                Node.fromJSON(newState.schema, block)
+              )
+              .delete(
+                blockPosition.pos - blockPosition.parentOffset - 1,
+                blockPosition.pos
+              );
+          } else {
+            // Because next page is not existed, we need to create a new page and move the current block
+            const blockPosition = transaction.selection.$from;
+            const block = blockPosition.parent.toJSON();
+            const newPage = pageSchema.nodes['page'].create(
+              { 'page-number': nextPageNumber },
+              [Node.fromJSON(newState.schema, block)]
+            );
+            nextPageTransaction = newState.tr.insert(
+              newState.doc.content.size,
+              newPage
+            );
+            // .delete(
+            //   blockPosition.pos - blockPosition.parentOffset - 1,
+            //   blockPosition.pos
+            // );
+          }
+        }
+        // having next page
+        if (nextPagePosition > 0) {
+          // MOVING BLOCK
+          // const blockPosition = transaction.selection.$from;
+          // const block = blockPosition.parent.toJSON();
+          // return newState.tr
+          //   .insert(nextPagePosition + 1, Node.fromJSON(newState.schema, block))
+          //   .delete(
+          //     blockPosition.pos - blockPosition.parentOffset - 1,
+          //     blockPosition.pos
+          //   );
+        } else {
+          // add new page.
+        }
+
+        //console.log('transaction.doc.textContent', transaction.doc.node.textContent);
+        //console.log('page 2', newState.tr.doc.children[1].resolve(newState.));
+        //console.log('doc', newState.doc.resolve)
+        //newState.doc.children[2].
+        // console.log('doc.child(1)', newState.doc.child(1));
+        // console.log('doc.nodeAt(1)', newState.doc.nodeAt(1));
+        // // console.log(
+        // //   'doc.nodeAt($form.pos)',
+        // //   newState.doc.nodeAt(transaction.selection.$from)
+        // // );
+        // console.log('nodeSize', newState.doc.nodeSize);
+        // console.log('content.size', newState.doc.content.size);
+        // if (newState.doc.textContent.includes('insert')) {
+        //   return newState.tr.insert(
+        //     newState.doc.content.size - 3,
+        //     newState.schema.nodes['paragraph'].create(null, [
+        //       newState.schema.text('datrtruong'),
+        //     ])
+        //   );
+        // }
+
+        // newState.doc.descendants((node, pos) => {
+        //   if (node.type.name === 'paragraph') {
+        //     // Detect the position of the page node that has been modified
+        //     // (Check if the current node was affected by the transaction)
+        //     const newPositionParagraph = transaction.mapping.map(pos);
+        //     newState.doc.descendants((parentNode, parentPos) => {
+        //       if (
+        //         parentNode.type.name === 'page' &&
+        //         newPositionParagraph >= parentPos &&
+        //         newPositionParagraph <= parentPos + parentNode.nodeSize
+        //       ) {
+        //         currentPage = parentNode;
+        //         pagePosition = parentPos;
+        //         return; // Stop the search
+        //       }
+        //     });
+        //   }
+        // });
+        // if (!currentPage) {
+        //   return;
+        // }
+        // console.log('currentPage', currentPage);
+        // if (!editorView) {
+        //   return null;
+        // }
+        // const pageDOM = editorView.dom.querySelector(
+        //   `div[page-number='${currentPage.attrs['page-number']}']`
+        // ) as any;
+        // console.log(pageDOM);
+
+        // const scrollHeight = pageDOM.scrollHeight;
+        // const clientHeight = pageDOM.clientHeight;
+
+        // console.log('scrollHeight', scrollHeight, 'clietHeight', clientHeight);
+        // if (scrollHeight > clientHeight) {
+        //   console.log('page overflow');
+
+        //   const newPage = pageSchema.nodes['page'].create(
+        //     { 'page-number': 2 },
+        //     [pageSchema.nodes['paragraph'].create()]
+        //   );
+        //   const newPositionBlock = transaction.selection.$from.pos;
+        //   console.log('index', newPositionBlock);
+        //   console.log(
+        //     'before',
+        //     transaction.selection.$from.before(newPositionBlock!)
+        //   );
+        //   const block = editorView.nodeDOM(
+        //     transaction.selection.$from.before(newPositionBlock!)
+        //   );
+        //   console.log('block', block);
+
+        //   //transaction.doc.resolve(transaction.selection.$from.parent)
+
+        //   //let newTransaction= newState.tr.insert(newState.doc.content.size, newPage) ;
+        //   // TODO: move block
+        //   //newTransaction.
+        //   //return newTransaction;
+        // }
+        // if (transaction.doc.textContent.includes('break page')) {
+        //   const newPage = pageSchema.nodes['page'].create({ 'page-number': 2 }, [
+        //     pageSchema.nodes['paragraph'].create(),
+        //   ]);
+        //   const position = newState.doc.content.size;
+        //   let trWithPage = newState.tr
+        //     // delete break page
+        //     .delete(
+        //       transaction.selection.$from.pos - 'break page'.length,
+        //       transaction.selection.$from.pos
+        //     )
+        //     // add new page
+        //     .insert(position - 'break page'.length, newPage);
+        //   console.log(trWithPage);
+        //   // Return the updated transaction (apply this change)
+        //   return trWithPage;
+        // } else {
+        //   console.log('no break');
+        // }
+      }
+      return null;
+    },
+  });
+}
