5ff0d40994c87cf77fc9ae11c1f1b007a8d6fa97
dat truong <thelegendaglx@gmail.com>
2025-05-17 18:28:17 +0700

    repaginate and change in the same transaction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { AfterViewInit, Component } from '@angular/core';
-import { EditorState } from 'prosemirror-state';
+import {
+  EditorState,
+  Selection,
+  TextSelection,
+  Transaction,
+} from 'prosemirror-state';
 import { EditorView } from 'prosemirror-view';
 import { Schema, DOMParser, NodeSpec } from 'prosemirror-model';
 import { schema } from 'prosemirror-schema-basic';
@@ -9,7 +14,140 @@ import { pageNodeSpec } from './prosemirror-nodes/page';
 import { pageBreakPlugin } from './prosemirror-plugin/page-break-plugin';
 import { pageSchema } from './prosemirror-schema/page-schema';
 import { Node } from 'prosemirror-model';
-import { pageBreakPlugin2 } from './prosemirror-plugin/page-break-plugin2';
+import {
+  pageBreakPlugin2,
+  repaginate,
+  repaginate2,
+} from './prosemirror-plugin/page-break-plugin2';
+import { MapResult, ReplaceStep, StepMap } from 'prosemirror-transform';
+
+class TransactionType {
+  type: 'UNKNOWN' | 'INSERT' | 'DELETE' | 'CLICK' | 'SELECT' = 'UNKNOWN';
+  insertText?: string; // This only has if type = INSERT
+}
+
+function isClickTransaction(transaction: Transaction) {
+  return (
+    transaction.steps &&
+    transaction.steps.length === 0 &&
+    transaction.selection.from === transaction.selection.to
+  );
+}
+function isSelectTransaction(transaction: Transaction) {
+  return (
+    transaction.steps &&
+    transaction.steps.length === 0 &&
+    transaction.selection.from !== transaction.selection.to
+  );
+}
+
+function isInsertTransaction(transaction: Transaction) {
+  return (
+    transaction.steps &&
+    transaction.step.length > 0 &&
+    transaction.steps[0] instanceof ReplaceStep &&
+    transaction.steps[0].from === transaction.steps[0].to
+  );
+}
+function isDeleteTransaction(transaction: Transaction) {
+  return (
+    transaction.steps &&
+    transaction.step.length > 0 &&
+    transaction.steps[0] instanceof ReplaceStep &&
+    transaction.steps[0].from < transaction.steps[0].to
+  );
+}
+
+function getInsertText(transaction: Transaction) {
+  const step = transaction.steps[0] as ReplaceStep | undefined;
+  return step?.slice.content.textBetween(0, step.slice.content.size) || '';
+}
+
+/**
+ * - INSERT type could be PASTE
+ * - DELETE type could be CUT
+ * @param transaction
+ * @returns
+ */
+function getTransactionType(transaction: Transaction): TransactionType {
+  if (!transaction.steps) {
+    return { type: 'UNKNOWN' };
+  }
+
+  if (isClickTransaction(transaction)) {
+    return { type: 'CLICK' };
+  }
+
+  if (isSelectTransaction(transaction)) {
+    return { type: 'SELECT' };
+  }
+
+  if (isInsertTransaction(transaction)) {
+    return { type: 'INSERT', insertText: getInsertText(transaction) };
+  }
+
+  if (isDeleteTransaction(transaction)) {
+    return { type: 'DELETE' };
+  }
+
+  return { type: 'UNKNOWN' };
+}
+
+interface TransactionDetail {
+  page?: Node;
+  pageStartOutsidePosition: number;
+  pageEndOutsidePosition: number;
+  pageStartInsidePosition: number;
+  pageEndInsidePosition: number;
+  nextPage?: Node;
+  block?: Node;
+  blockStartOutsidePosition: number;
+  blockEndOutsidePosition: number;
+  blockStartInsidePoistion: number;
+  blockEndInsidePosition: number;
+}
+
+function getTransactionDetail(transaction: Transaction): TransactionDetail {
+  const { selection } = transaction,
+    { depth } = selection.$from;
+  const pageDepth = 1;
+
+  const page = selection.$from.node(pageDepth);
+  const pageStartOutside = selection.$from.start(pageDepth); // first position at outside dom element
+  const pageEndOutside = selection.$from.end(pageDepth); // last position at oside dom element
+  const pageStartInside = selection.$from.before(pageDepth); // first position at inside dom element
+  const pageEndInside = selection.$from.after(pageDepth); // last posistion at inside dom element
+
+  const block = selection.$from.node(depth);
+  const blockStartOutside = selection.$from.start(depth); // first position at outside dom element
+  const blockEndOutside = selection.$from.end(depth); // last position at oside dom element
+  const blockStartInside = selection.$from.before(depth); // first position at inside dom element
+  const blockEndInside = selection.$from.after(depth); // last posistion at inside dom element
+
+  const pageIndex = selection.$from.index(pageDepth);
+  const nextPage =
+    transaction.doc.children.length > pageIndex + 1
+      ? transaction.doc.child(pageIndex + 1)
+      : undefined;
+
+  const detail: TransactionDetail = {
+    page: page,
+    pageStartOutsidePosition: pageStartOutside,
+    pageEndOutsidePosition: pageEndOutside,
+    pageStartInsidePosition: pageStartInside,
+    pageEndInsidePosition: pageEndInside,
+    nextPage: nextPage,
+    block: block,
+    blockStartOutsidePosition: blockStartOutside,
+    blockEndOutsidePosition: blockEndOutside,
+    blockStartInsidePoistion: blockStartInside,
+    blockEndInsidePosition: blockEndInside,
+  };
+
+  console.log(detail);
+  return detail;
+}
+
 @Component({
   selector: 'app-root',
   imports: [],
@@ -17,6 +155,7 @@ import { pageBreakPlugin2 } from './prosemirror-plugin/page-break-plugin2';
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements AfterViewInit {
+  findNextBlock() {}
   exportJSon() {
     console.log(this.view?.state.doc.toJSON());
   }
@@ -42,7 +181,8 @@ export class AppComponent implements AfterViewInit {
               content: [
                 {
                   type: 'text',
-                  text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. blandit lorem in auctor egestas. In vehicula, lorem ac tempus lacinia, velit mauris dignissim odio, venenatis faucibus eros lorem accumsan mauris. Maecenas risus nisl, aliquet id mi eu, iaculis mattis ante. Aliquam nec tincidunt felis, vitae venenatis ligula. Integer eleifend, justo ac mattis sagittis, tortor lorem ornare nibh, at tempor enim nisi vitae diam. Praesent venenatis commodo orci a tristique. Nulla ex nibh, laoreet ut quam quis, rhoncus faucibus magna.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam blandit arcu ut odio iaculis, eu facilisis massa varius. Praesent dolor nibh, laoreet ac nisi id, placerat gravida justo. Quisque ac felis dictum nulla ornare posuere. Interdum et malesuada fames ac ante ipsum primis in faucibus. Quisque pretium lacinia justo quis hendrerit. Phasellus tortor risus, dapibus sed nunc ut, lacinia lobortis turpis. Praesent porttitor leo at libero dapibus auctor. Sed ac molestie tellus. Etiam ultrices magna quis pulvinar blandit. Phasellus bibendum consequat tincidunt. Quisque sed sem sed lacus volutpat placerat. Sed nisi lacus, aliquet ac lobortis ut, iaculis quis ipsum. Nam sagittis tortor sit amet scelerisque vulputate. Aenean mollis et enim at ultricies. Etiam ac sollicitudin elit, eget sollicitudin ligula. Integer id urna vehicula, cursus mauris sit amet, tristique lacus. Nunc faucibus ex nec enim dapibus vehicula. Nunc at leo laoreet, pharetra lacus ut, ultricies urna. Cras vitae finibus tellus. Praesent quis nunc id purus gravida tempor. Fusce ac felis venenatis, luctus arcu elementum, tempor neque. Nullam ut erat id purus tincidunt dictum. Curabitur imperdiet vel augue nec ornare. In hac habitasse platea dictumst. Proin pulvinar augue sapien, eget porttitor tellus pharetra ac. Nam sit amet nunc et lectus sodales dictum non ut leo. Maecenas vestibulum justo nec lacinia rutrum. Maecenas in fermentum lectus, sed aliquam ipsum. Aliquam interdum sollicitudin mi, vel placerat magna commodo et. Nunc sit amet est mauris. Nam accumsan ullamcorper tortor et mollis. Nunc accumsan est nec varius pulvinar. Vivamus feugiat volutpat tortor non volutpat. Mauris et felis in justo porttitor maximus faucibus sed enim. Etiam imperdiet sed turpis et vulputate. Morbi posuere ex nec dui vulputate, sed consequat felis venenatis. Duis consectetur sodales arcu. Pellentesque laoreet malesuada nunc nec rutrum. Suspendisse tincidunt sed nunc vitae varius. Ut pellentesque ipsum ac pulvinar porttitor. Duis et elit a odio auctor venenatis. Curabitur sit amet leo et magna fermentum accumsan. Pellentesque interdum hendrerit ex et vulputate. Suspendisse condimentum aliquet mi at placerat. Aliquam dapibus magna ut urna euismod, congue condimentum nibh scelerisque. Nam iaculis lobortis nisi, nec facilisis lorem rhoncus nec. Aenean blandit lorem in auctor egestas. In vehicula, lorem ac tempus lacinia, velit mauris dignissim odio, venenatis faucibus eros lorem accumsan mauris. Maecenas risus nisl, aliquet id mi eu, iaculis mattis ante. Aliquam nec tincidunt felis, vitae venenatis ligula. Integer eleifend, justo ac mattis sagittis, tortor lorem ornare nibh, at tempor enim nisi vitae diam. Praesent venenatis commodo orci a tristique. Nulla ex nibh, laoreet ut quam quis, rhoncus faucibus magna.',
+                  text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. blandit lorem in auctor egestas. In vehicula, lorem ac tempus lacinia, velit mauris dignissim odio, venenatis faucibus eros lorem accumsan mauris. Maecenas risus nisl, aliquet id mi eu, iaculis mattis ante. Aliquam nec tincidunt felis, vitae venenatis ligula. Integer eleifend, justo ac mattis sagittis, tortor lorem ornare nibh, at tempor enim nisi vitae diam. Praesent venenatis commodo orci a tristique. Nulla ex nibh, laoreet ut quam quis, rhoncus faucibus magna.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam blandit arcu ut odio iaculis, eu facilisis massa varius. Praesent dolor nibh, laoreet ac nisi id, placerat gravida justo. Quisque ac felis dictum nulla ornare posuere. Interdum et malesuada fames ac ante ipsum primis in faucibus. Quisque pretium lacinia justo quis hendrerit. Phasellus tortor risus, dapibus sed nunc ut, lacinia lobortis turpis. Praesent porttitor leo at libero dapibus auctor. Sed ac molestie tellus. Etiam ultrices magna quis pulvinar blandit. Phasellus bibendum consequat tincidunt. Quisque sed sem sed lacus volutpat placerat. Sed nisi lacus, aliquet ac lobortis ut, iaculis quis ipsum. Nam sagittis tortor sit amet scelerisque vulputate. Aenean mollis et enim at ultricies. Etiam ac sollicitudin elit, eget sollicitudin ligula. Integer id urna vehicula, cursus mauris sit amet, tristique lacus. Nunc faucibus ex nec enim dapibus vehicula. Nunc at leo laoreet, pharetra lacus ut, ultricies urna. Cras vitae finibus tellus. Praesent quis nunc id purus gravida tempor. Fusce ac felis venenatis, luctus arcu elementum, tempor neque. Nullam ut erat id purus tincidunt dictum. Curabitur imperdiet vel augue nec ornare. In hac habitasse platea dictumst. Proin pulvinar augue sapien, eget porttitor tellus pharetra ac. Nam sit amet nunc et lectus sodales dictum non ut leo. Maecenas vestibulum justo nec lacinia rutrum. Maecenas in fermentum lectus, sed aliquam ipsum. Aliquam interdum sollicitudin mi, vel placerat magna commodo et. Nunc sit amet est mauris. Nam accumsan ullamcorper tortor et mollis. Nunc accumsan est nec varius pulvinar. Vivamus feugiat volutpat tortor non volutpat. Mauris et felis in justo porttitor maximus faucibus sed enim. Etiam imperdiet sed turpis et vulputate. Morbi posuere ex nec dui vulputate, sed consequat felis venenatis. Duis consectetur sodales arcu. Pellentesque laoreet malesuada nunc nec rutrum. Suspendisse tincidunt sed nunc vitae varius. Ut pellentesque ipsum ac pulvinar porttitor. Duis et elit a odio auctor venenatis. Curabitur sit amet leo et magna fermentum accumsan. Pellentesque interdum hendrerit ex et vulputate. Suspendisse condimentum aliquet mi at placerat. Aliquam dapibus magna ut urna euismod, congue condimentum nibh scelerisque. Nam iaculis lobortis nisi, nec facilisis lorem rhoncus nec. Aenean blandit lorem in auctor egestas. In vehicula, lorem ac tempus lacinia, velit mauris dignissim odio, venenatis faucibus eros lorem accumsan mauris. Maecenas risus nisl, aliquet id mi eu, iaculis mattis ante. Aliquam nec tincidun',
+                  //text: '',
                 },
               ],
             },
@@ -69,19 +209,90 @@ export class AppComponent implements AfterViewInit {
         // ),
         plugins: [
           ...exampleSetup({ schema: this.mySchema }),
-          pageBreakPlugin2(this.view),
+          //pageBreakPlugin2(this.view),
         ],
       }),
+      dispatchTransaction: (tr: Transaction) => {
+        const view = this.view!,
+          { state } = view,
+          newTransaction = state.tr;
+        const originPosition = tr.selection.to;
+
+        if (tr.docChanged) {
+          const transaction = repaginate2(tr);
+          if (transaction) {
+            console.log(
+              'cursor move from ',
+              originPosition,
+              ' to ',
+              transaction.mapping.map(originPosition)
+            );
+
+            let newCursorPosition = originPosition;
+            const transactionType = getTransactionType(tr);
+            if (transactionType.type === 'INSERT') {
+              newCursorPosition += transactionType.insertText!.length;
+            }
+            let selectionTransaction: Transaction | undefined;
+
+            // if (newCursorPosition < transaction.doc.nodeSize) {
+            //   const textSelection = TextSelection.create(
+            //     transaction.doc,
+            //     newCursorPosition
+            //   );
+            //   selectionTransaction = transaction.setSelection(textSelection);
+            // }
+
+            const newSelection = Selection.findFrom(
+              transaction.doc.resolve(originPosition),
+              1,
+              true
+            );
+            if (newSelection) {
+              selectionTransaction = transaction.setSelection(newSelection);
+            }
+
+            // TODO: update selection text (cursor)
+            view.updateState(
+              view.state.apply(
+                selectionTransaction ? selectionTransaction : transaction
+              )
+            );
+            return;
+          }
+        } else {
+          console.log(
+            'cursor move from ',
+            originPosition,
+            ' to ',
+            tr.mapping.map(originPosition)
+          );
+        }
+        const detail = getTransactionDetail(tr);
+
+        // Changing position of transaction if it has steps (insert/delete)
+        //console.log(transactionType);
+
+        // if (transactionType.type == 'INSERT' && transactionType.insertText && transactionType.insertText.length > 10)  {
+        //   let index= 0;
+        //   for (index = 0; index < transactionType.insertText.length; index++) {
+        //     const element = array[index];
+
+        //   }
+        //   if (index < transactionType.insertText.length <)
+        // }
+        view.updateState(state.apply(tr));
+      },
     });
 
-    document.addEventListener('click', (event) => {
-      console.log(event.target);
+    // document.addEventListener('click', (event) => {
+    //   console.log(event.target);
 
-      console.log(
-        'posAtDOM: ',
-        this.view?.posAtDOM(event.target as any, 0, -1)
-      );
-    });
+    //   console.log(
+    //     'posAtDOM: ',
+    //     this.view?.posAtDOM(event.target as any, 0, -1)
+    //   );
+    // });
   }
 
   insertNewPage() {
--- src/app/prosemirror-plugin/page-break-plugin2.ts
+++ src/app/prosemirror-plugin/page-break-plugin2.ts
@@ -3,9 +3,70 @@ import { pageSchema } from '../prosemirror-schema/page-schema';
 import { EditorView } from 'prosemirror-view';
 import { DOMSerializer, Fragment, Node, Schema } from 'prosemirror-model';
 import { nodes } from 'prosemirror-schema-basic';
+export function repaginate2(transaction: Transaction, pageHeightPx = 700) {
+  // Flatten all blocks
+  const blocks: Node[] = [];
+  transaction.doc.forEach((pageNode) => {
+    pageNode.forEach((block) => {
+      blocks.push(block);
+    });
+  });
+  // Create temporary container for measurement
+  let tempContainer = document.getElementById('pm-pagination-measure');
+  if (!tempContainer) {
+    tempContainer = document.createElement('div');
+    tempContainer.id = 'pm-pagination-measure';
+    tempContainer.style.cssText = `
+            position: absolute;
+            visibility: hidden;
+            top: 0;
+            left: 0;
+            width: 595px;
+            padding: 20px;
+            box-sizing: border-box;
+            z-index: -1;
+        `;
+    document.body.appendChild(tempContainer);
+  }
+  const measured = measureBlockHeights(blocks, pageSchema, tempContainer);
+  const pages = [];
+  let current = [];
+  let currentHeight = 0;
+  for (const { block, height } of measured) {
+    if (currentHeight + height > pageHeightPx && current.length > 0) {
+      pages.push(current);
+      current = [];
+      currentHeight = 0;
+    }
+    current.push(block);
+    currentHeight += height;
+  }
 
-function repaginate(view: EditorView, pageHeightPx = 700) {
-  const { state } = view;
+  if (current.length > 0) {
+    pages.push(current);
+  }
+  const pageNodes = pages.map((blocks, index) =>
+    pageSchema.nodes['page'].createAndFill(
+      { 'page-number': ++index },
+      Fragment.fromArray(blocks)
+    )
+  );
+  const newDoc = pageSchema.nodes['doc'].createAndFill(
+    null,
+    Fragment.fromArray(pageNodes.filter((pageNode) => !!pageNode))
+  );
+  if (!newDoc || newDoc.eq(transaction.doc)) {
+    return;
+  }
+
+  const tr = transaction.replaceWith(
+    0,
+    transaction.doc.content.size,
+    newDoc.content
+  );
+  return tr;
+}
+export function repaginate(state: EditorState, pageHeightPx = 700) {
   const { schema } = state;
   // Flatten all blocks
   const blocks: Node[] = [];
@@ -31,7 +92,7 @@ function repaginate(view: EditorView, pageHeightPx = 700) {
         `;
     document.body.appendChild(tempContainer);
   }
-  const measured = measureBlockHeights(blocks, schema, tempContainer, view);
+  const measured = measureBlockHeights(blocks, schema, tempContainer);
   const pages = [];
   let current = [];
   let currentHeight = 0;
@@ -63,14 +124,13 @@ function repaginate(view: EditorView, pageHeightPx = 700) {
   }
 
   const tr = state.tr.replaceWith(0, state.doc.content.size, newDoc.content);
-  view.dispatch(tr);
+  return tr;
 }
 
-function measureBlockHeights(
+export function measureBlockHeights(
   blocks: Node[],
   schema: Schema,
-  container: HTMLElement,
-  editorView: EditorView
+  container: HTMLElement
 ) {
   container.innerHTML = ''; // Clear previous content
   const domSerializer = DOMSerializer.fromSchema(schema);
@@ -79,7 +139,11 @@ function measureBlockHeights(
   blocks.forEach((block) => {
     const dom = domSerializer.serializeNode(block) as Element;
     container.appendChild(dom);
-    const height = dom.getBoundingClientRect().height + 16; // because for each p has margin 16px
+    let height = dom.getBoundingClientRect().height;
+    if (height === 0) {
+      height = 18;
+    }
+    height += 16; // because for each p has margin 16px
 
     heights.push({ block, height });
   });
@@ -97,7 +161,10 @@ export function pageBreakPlugin2(view: EditorView | undefined) {
         update(view, prevState) {
           if (view.state.doc.eq(prevState.doc)) {
             console.log('view plugin running');
-            repaginate(view);
+            const tr = repaginate(view.state);
+            if (tr) {
+              view.dispatch(tr);
+            }
           }
         },
       };
